38806d4c4c92943df445f074cc5ae7af8a81a6d3
spradhan7656 <spradhan7656@gmail.com>
2023-11-16 17:32:58 +0530

    add some comment

--- src/test/java/com/utils/CommonUtils.java
+++ src/test/java/com/utils/CommonUtils.java
@@ -8,12 +8,22 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+/*
+ * <p>This class consists of Re-usable helper/ utility method</p>
+ * @ Author -Santosh Prahan
+ * @ Version -2.0
+ * @See External Referances
+ * 
+ * 
+ */
 
 public class CommonUtils 
 {
 	public Properties configData;
 	public WebDriver driver;
-	
+	/*
+	 * <p> This method read the config and parses the same into key value pairs </p>
+	 */
 	public void readConfig()
 	{
 		
@@ -33,6 +43,9 @@ public class CommonUtils
 		}
 	}
 	
+	/*
+	 * <p> This method is use to lunch the browser</p>
+	 */
 	public void lunchBrowser()
 	{
 		if(configData.getProperty("browser.name").equalsIgnoreCase("chrome"))
@@ -47,6 +60,9 @@ public class CommonUtils
 			//implement of other block
 		}
 	}
+	/*
+	 * <p> This method is use to quit the browser</p>
+	 */
 	public void closeBrowser()
 	{
 		driver.quit();
